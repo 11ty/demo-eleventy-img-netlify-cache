a988f3eeb95021f44dd57cc7da643725a92cf0bc
Zach Leatherman <zachleatherman@gmail.com>
2022-02-24 12:18:51 -0600

    Code cleanup

--- plugins/custom-cache/index.js
+++ plugins/custom-cache/index.js
@@ -1,18 +1,20 @@
+const IMAGE_DIRECTORY = "./_site/img/generated";
+const IMAGE_TTL_SECONDS = 60 * 60 * 24 * 7; // 7 days
+
 module.exports = {
   // Restore file/directory cached in previous builds.
   // Does not do anything if:
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
-    await utils.cache.restore('./_site/img/generated');
+    await utils.cache.restore(IMAGE_DIRECTORY);
   },
   // Cache file/directory for future builds.
   // Does not do anything if:
   //  - the file/directory does not exist locally
   async onPostBuild({ utils }) {
-    await utils.cache.save('./_site/img/generated', {
-       // value in seconds
-      ttl: 60 * 60 * 24 * 7 // 7 days
+    await utils.cache.save(IMAGE_DIRECTORY, {
+      ttl: IMAGE_TTL_SECONDS
     });
   }
 };
